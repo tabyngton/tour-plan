8f76bb6928695c456aa1eec17c54c0f14a6e30db
tabyngton <48449816+tabyngton@users.noreply.github.com>
2020-10-22 15:19:27 +0300

    create:  added swiper-slider in hotel section

--- js/main.js
+++ js/main.js
@@ -2,19 +2,9 @@ var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
   },
 })
